60a9f4c88b58273add9a20e8005f232a106c7269
Erik Riquelme <erikone2311@gmail.com>
2024-10-19 22:50:28 +0200

    Move createBoard test to board folder

---
+++ src/board/__tests__/createBoard.test.ts
@@ -0,0 +1,30 @@
+import { createBoard } from "../index";
+
+describe("Given the createBoard function", () => {
+  describe("When it recives 8", () => {
+    test("Then it should return board with 8 rows and 8 boxes in each row", () => {
+      const boardSize = 7;
+      const expectedRows = 7;
+      const expectedBoxes = 7;
+
+      const rows = createBoard(boardSize);
+
+      expect(rows.length).toBe(expectedRows);
+      expect(rows.at(1)?.length).toBe(expectedBoxes);
+    });
+  });
+
+  describe("When it's called", () => {
+    test("Then it should return a board where each of its cell has not mines around", () => {
+      const boardSize = 6;
+      const expectedMinesAroundTotal = 0;
+
+      const board = createBoard(boardSize);
+      const allBoxesHaveZeroMines = board.every((row) =>
+        row.every((box) => box.minesAroundTotal === expectedMinesAroundTotal)
+      );
+
+      expect(allBoxesHaveZeroMines).toBeTruthy();
+    });
+  });
+});
